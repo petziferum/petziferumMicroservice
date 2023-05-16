e6b906ea912c04076f79cc8bcfe167e2647793fa
petziferum <daboarderpjb@gmail.com>
2023-05-16 22:25:27 +0200

    Restcontroller

--- eventservice/src/main/java/com/petziferum/eventservice/event/EventController.java
+++ eventservice/src/main/java/com/petziferum/eventservice/event/EventController.java
@@ -2,12 +2,10 @@ package com.petziferum.eventservice.event;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController("/events")
+@RestController
+@RequestMapping("events")
 public class EventController {
 
     @PostMapping("/new")
